110f5f70b9c480213e8731a1b1f29d398a6db0d0
Ruslan <Ruslan90.0843@gmail.com>
2022-12-12 23:38:45 +0400

    Ended Task_01

---
+++ Home_Work_04/Task_01/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+
+degreeNumberA(getNumberOne(),getNumberTwo());
+
+
+static string getMessege(string str) {
+    Console.WriteLine($"Введите {str} число");
+    string messege = Console.ReadLine()??"";
+    return messege; 
+}
+
+static int getNumberOne() {
+    int number = 0;
+    while(!(int.TryParse(getMessege("первое")??"",out number)) || number == 0) {
+        Console.WriteLine("Вы ввели не верный формат , число не должно быть равно нулю");
+    }
+    return number;
+}
+
+static int getNumberTwo() {
+    int number = 0;
+    while(!(int.TryParse(getMessege("второе")??"",out number)) || number < 0 || number == 0) {
+        Console.WriteLine("Вы ввели не верный формат , число не должно быть меньше либо равно нулю");
+    }
+    return number;
+}
+
+static void degreeNumberA(int numberA, int numberB) {
+    int number = numberA;
+    for (int i = 1; i < numberB; i++)
+    {
+        number = number * numberA;
+        Console.WriteLine(number);
+    }
+    Console.WriteLine($"число {numberA} в степени {numberB} = {number}");
+}
