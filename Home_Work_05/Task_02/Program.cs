805b7dd83f610fec2346bb1edf6847f993e7bebf
Ruslan <Ruslan90.0843@gmail.com>
2022-12-19 16:40:10 +0400

    Ended Task_02

---
+++ Home_Work_05/Task_02/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+
+// [3, 7, 23, 12] -> 19
+
+// [-4, -6, 89, 6] -> 0
+
+Console.WriteLine(summOddElements());
+
+static int summOddElements() {
+    int[] arr = fillByArr(greatArr(getLengthByArr()));
+    int summ = 0;
+    for (int i = 1; i < arr.Length; i++)
+    {
+        if (i % 2 != 0)
+        {
+            summ =+ arr[i];
+        }
+    }
+    return summ;
+}
+
+static int getLengthByArr() {
+    
+    int lengthArr = new Random().Next(1,101);
+    return lengthArr;
+}
+
+static int[] fillByArr(int[] arr) {
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(1,101);
+    }
+    return arr;
+}
+
+static int[] greatArr(int length) {
+    return new int[length];
+}
